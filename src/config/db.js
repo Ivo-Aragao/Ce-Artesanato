21e7104e372fb8c6469d37798af6d94e10180662
ivo aragao <ivoaragao4@gmail.com>
2025-03-20 21:21:52 -0300

    feat apagando senha

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 module.exports = new Pool({
   user: 'postgres',
-  password: 'Senhordosaneis#1',
+  password: 'COLOQUE SUA SENHA',
   host: 'localhost',
   port: 5432,
   database: 'launchstore'
